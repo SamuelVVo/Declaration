9841a0fba056e2f4769badd0828dd68d82f05b50
SamuelVVo <116473504+SamuelVVo@users.noreply.github.com>
2024-08-22 16:40:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const yesBtn = document.querySelector('#yesBtn')
 yesBtn.addEventListener('click', ()=>{
-  alert('Bueno, creo que vos ya lo sabes pero pues estoy mas o menos cerca de entrar en una relación, aunque aun no estoy del todo seguro, pero si sé que desde la ultima vez q hablamos tengo ganas de algo, y creo q la mejor manera es ser completamente directo. Ese algo que quiero es besarte, sin necesidad de relación ni nada, solo me gustaría pq hace rato q tengo ganas jajaja y pues me gustaría que si al fin me decido a tener una relación pues fuera antes de entrar en ella y antes de que te vayas, no sé si te gustaría, si no, y pues nada, era eso')
+  alert('Sabía que dirías que si, feliz primer dia de novios :3 jsjsjs')
 })
 
 const noBtn = document.querySelector('#noBtn')
